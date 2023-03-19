86e800aa517bb6af2bb8809a4b2a414ffbb73976
MathieuMMI <mathieu.bouque@edu.univ-fcomte.fr>
2023-03-20 00:11:21 +0100

    correc_lien_img

--- src/views/ParcoursView.vue
+++ src/views/ParcoursView.vue
@@ -5,7 +5,7 @@
           <h2 class="font-extrabold tracking-tight">Mon parcours</h2>
           <h2 class="text-2xl font-bold mb-4">Formation</h2>
           <p>oui</p>
-          <img src="public/img/lyceeartois.jpg" />
+          <img src="../../public/img/lyceeartois.jpg" />
           <h3 class="text-xl font-bold">Diplôme obtenu</h3>
           <p class="text-lg font-medium text-gray-700">Nom de l'établissement | Date d'obtention</p>
           <h3 class="text-2xl font-bold mb-4">Compétences</h3>
