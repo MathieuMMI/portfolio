acec55d7dae1972589d468dddcd80fc0954c1728
MathieuMMI <mathieu.bouque@edu.univ-fcomte.fr>
2023-12-19 21:40:08 +0100

    feat:suppression liens rapides

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,6 @@ import Footer from "@/components/layout/Footer.vue"
 <template>
   <header>
     <Header />
-    <router-link to="/Swoss">Swoss</router-link>
-    <router-link to="/Argimes">Argimes</router-link>
-    <router-link to="/ChartJS">ChartJS</router-link>
-    <router-link to="/Firebase">Firebase</router-link>
-    <router-link to="/Prismic">Prismic</router-link>
-    <router-link to="/Supabase">Supabase</router-link>
-    <router-link to="/UC">UC</router-link>
-    <router-link to="/UE">UE</router-link>
-    <router-link to="/VR">VR</router-link>
   </header>
   <main class="mb-36">
     <RouterView />
