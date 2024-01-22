5abc80410b7906a81b9664bf7a66a7cc482a2318
MathieuMMI <mathieu.bouque@edu.univ-fcomte.fr>
2024-01-22 17:12:47 +0100

    feat: responsive

--- src/components/layout/Menu.vue
+++ src/components/layout/Menu.vue
@@ -3,11 +3,38 @@
         <div>
             <router-link to="/" class="text-xl font-bold">Accueil</router-link>
         </div>
-        <div>
-            <router-link to="/Projets" class="mr-4">Mes projets</router-link>
-            <router-link to="/Etudes" class="mr-4">Mes études</router-link>
-            <router-link to="/APropos" class="mr-4">À propos</router-link>
+        <div v-if="!isMobile">
+            <router-link to="/Projets" class="md:mr-4 m-0">Mes projets</router-link>
+            <router-link to="/Etudes" class="md:mr-4 m-0">Mes études</router-link>
+            <router-link to="/APropos" class="md:mr-4 m-0">À propos</router-link>
+        </div>
+        <div v-if="isMobile">
+            <button @click="toggleMenu" class="text-xl">
+                ☰
+            </button>
+            <div v-if="menuOpen" class="absolute top-16 left-0 right-0 bg-gray-800 text-white p-4">
+                <router-link to="/Projets" @click="toggleMenu" class="block mb-2">Mes projets</router-link>
+                <router-link to="/Etudes" @click="toggleMenu" class="block mb-2">Mes études</router-link>
+                <router-link to="/APropos" @click="toggleMenu" class="block mb-2">À propos</router-link>
+            </div>
         </div>
     </nav>
 </template>
+  
+<script setup>
+import { ref } from 'vue';
+
+const isMobile = ref(window.innerWidth <= 768);
+const menuOpen = ref(false);
+
+const toggleMenu = () => {
+    menuOpen.value = !menuOpen.value;
+};
+
+// Met à jour isMobile lorsqu'il y a un redimensionnement de la fenêtre
+window.addEventListener('resize', () => {
+    isMobile.value = window.innerWidth <= 768;
+});
+
+</script>
   
--- src/views/Accueil.vue
+++ src/views/Accueil.vue
@@ -1,25 +1,26 @@
 <template>
   <div class="container mx-auto my-8 text-center mt-36">
-    <h1 class="text-4xl font-bold mb-2">Mathieu BOUQUE</h1>
-    <h2 class="text-3xl text-gray-600 mb-4">Étudiant en troisième année de BUT MMI, parcours DWDI</h2>
-    <p class="text-lg text-gray-700 text-left mr-64 ml-64">Actuellement à la recherche d'un stage pour ma dernière année
+    <h1 class="text-3xl md:text-4xl font-bold mb-2">Mathieu BOUQUE</h1>
+    <h2 class="text-2xl md:text-3xl text-gray-600 mb-4">Étudiant en troisième année de BUT MMI, parcours DWDI</h2>
+    <p class="text-lg text-gray-700 text-left ml-10 mr-10 mb-10">Actuellement à la recherche d'un stage pour ma dernière année
       en BUT MMI à Montbéliard. Passionné par les jeux vidéos et les nouvelles technologies, je recherche un stage pour
       une durée de 16 semaines à partir du 19 février 2024. J’ai eu l’occasion d’utiliser diverses technologies et de les
       appliquer sur des projets ou exercices dans le domaine du web et du jeu vidéo.</p>
-    <h2 class="text-3xl font-bold mb-20">Ils me recommandent</h2>
+    <h2 class="text-3xl font-bold mb-5 md:mb-20">Ils me recommandent</h2>
     <div class="mb-10">
       <h3 class="text-2xl font-bold mb-5">Pascal Chatonnay</h3>
-      <p class="text-lg text-gray-700 mb-5">Référent Défense et Sécurité Nationale (RDSN) de l'université de
+      <p class="text-lg text-center text-gray-700 mb-5 ml-10 mr-10">Référent Défense et Sécurité Nationale (RDSN) de l'université de
         Franche-Comté, professeur en MMI à Montbéliard.</p>
-      <figure><img src="/img/pchaton.jpg" alt="Photographie de mon carnet sur l'Intelligence Artificielle"
-          class="m-auto w-[25%]" />
-        <figcaption class="text-center">Mon carnet sur l'intelligence artificielle</figcaption>
+      <figure class="md:items-center mb-5">
+        <img src="/img/pchaton.jpg" alt="Photographie de Pascal Chatonnay"
+          class="m-auto w-48 " />
+        <figcaption class="text-center">Pascal Chatonnay</figcaption>
       </figure>
       <a href="https://www.linkedin.com/in/pascalchatonnay/" class="text-center">
         <p class="text-lg text-gray-700">Voir le Linkedin</p>
       </a>
     </div>
-    <button class="text-2xl text-gray-700 mb-5" @click="telechargerPDF()">Télécharger la lettre de recommandation de
+    <button class="text-xl text-gray-700 mb-5" @click="telechargerPDF()">Télécharger la lettre de recommandation de
       Pascal Chatonnay</button>
   </div>
 </template>
