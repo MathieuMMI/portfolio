7133bab204f462506ec93f9832064c178e82dc00
MathieuMMI <mathieu.bouque@edu.univ-fcomte.fr>
2024-03-02 14:57:46 +0100

    feat: uc jeu

--- src/views/Projets/UCjeu.vue
+++ src/views/Projets/UCjeu.vue
@@ -1,3 +1,71 @@
 <template>
-    <h2 class="text-5xl font-bold mb-5 text-center mt-52">Ce projet n'a pas encore sa description</h2>>
+    <div class="container mx-auto my-8">
+        <h1 class="text-4xl font-bold mb-4">Unified Champions - Jeu</h1>
+
+        <!-- Introduction -->
+        <div class="mb-8">
+            <p class="ml-5 mr-5 text-gray-700 text-left text-xl"></p>
+            <figure>
+                <img src="/img/ue5.png" alt="Logo d'Unreal Engine" class="rounded-md w-[25%] m-auto" />
+                <figcaption class="text-center">Logo d'Unreal Engine</figcaption>
+            </figure>
+        </div>
+
+        <div class="mb-12">
+            <h2 class="text-3xl font-bold mb-4 text-center">Résumé</h2>
+            <p class="ml-5 mr-5 text-gray-700 text-left text-xl mb-5">
+                Unified Champions est une association regroupant des joueurs et des équipes pour jouer à des jeux
+                esport, de
+                manière compétitive ou non. L'association proposait aux étudiants de s'investir via divers postes :
+                coach,
+                développeur, community manager, et bien d'autres.
+            </p>
+        </div>
+
+        <div class="mb-12">
+            <h2 class="text-3xl font-bold mb-4 text-center">Idée du jeu</h2>
+            <p class="ml-5 mr-5 text-gray-700 text-left text-xl mb-5">
+                Au cours du troisième semestre, à côté de la réalisation du site Wordpress, nous avions des réunions où
+                nous discutions de la possibilité de créer un jeu vidéo pour l'association. Puis nous avons réalisé
+                divers brainstorming, jusqu'à arriver à une première idée de jeu : un jeu de voiture où les contrôles
+                des voitures sont partagées entre deux personnes, puis trois, puis quatre, dans une sorte de tournois de
+                course ou d'affrontement entre véhicules. Le nombre de personnes contrôlant la voiture augmente de 1 à
+                chaque victoire. Nous avions à peu près défini la première idée de jeu qu'allait commencer mon stage de
+                deuxième année, que j'ai effectué à l'association.
+            </p>
+        </div>
+
+        <div class="mb-12">
+            <h2 class="text-3xl font-bold mb-4 text-center">Stage - Première version</h2>
+            <p class="ml-5 mr-5 text-gray-700 text-left text-xl mb-5">
+                Le stage a été pour moi l'occasion de découvrir l'usage des véhicules avec Unreal Engine 5, ainsi que la
+                manière dont on les utilise en tant que Personnage Non Joueur. De plus, j'ai aussi découvert la manière
+                dont on développe un jeu multijoueur, avec pour hébergeur le créateur de la partie. Toute cette partie a
+                été fort instructive pour moi.
+            </p>
+        </div>
+
+        <div class="mb-12">
+            <h2 class="text-3xl font-bold mb-4 text-center">Seconde version</h2>
+            <p class="ml-5 mr-5 text-gray-700 text-left text-xl mb-5">
+                Le terrain a été travaillé pour cette version, avec l'idée d'avoir un passage au dessus de la route
+                normale, un passage qui se déclenche au hasard et qui permet à un joueur d'obtenir un boost. Nous avons
+                continué les réunions régulières pendant que j'avançais le développement. de plus, un modèle 3D a été
+                réalisé par d'autres membres de l'association. L'idée du jeu a changé : nous avons choisi de partir sur
+                des mini-voitures, et nous devons trouver au début de chaque manche quelle voiture nous jouons (il y a
+                trois manches en total). La caméra est fixée sur les quatre voitures autour de nous, et il y a plusieurs
+                chemins : les voitures se rejoignent en bout de chemin.
+            </p>
+        </div>
+
+        <div class="mb-12">
+            <h2 class="text-3xl font-bold mb-4 text-center">Bilan</h2>
+            <p class="ml-5 mr-5 text-gray-700 text-left text-xl mb-5">
+                Le jeu n'a pas été mené à termes, l'objectif était pour moi de poser le prémisses du jeu pour que de
+                futurs membres se basent dessus pour pour ensuite le mener à termes. Il a pour objectif de participer à
+                la cohésion interne des membres de l'association.
+            </p>
+        </div>
+
+    </div>
 </template>
