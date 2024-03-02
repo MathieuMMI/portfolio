b7b5e6867a22069eca0b716db9a69844b7a972c3
MathieuMMI <mathieu.bouque@edu.univ-fcomte.fr>
2024-03-02 10:22:29 +0100

    feat: uc site

--- src/views/Projets/UC.vue
+++ src/views/Projets/UC.vue
@@ -27,10 +27,10 @@
     </div>
 
     <div class="mb-12">
-      <h2 class="text-3xl font-bold mb-4 text-center">Résumé</h2>
+      <h2 class="text-3xl font-bold mb-4 text-center">Création des sites</h2>
       <p class="ml-5 mr-5 text-gray-700 text-left text-xl mb-5">
-        Lors du second semestre, nous avons choisi de parti sur le site d'Unified Champions, c'est là mon premier contact
-        avec l'association.
+        Lors du second semestre, nous avons choisi de parti sur le site d'Unified Champions, c'est là mon premier
+        contact avec l'association.
         Trois groupes participaient : il fallait un site pour l'association, un site pour le club, qui présenterait
         l'activité des joueurs et de l'équipe principale, notre équipe League Of Legends, et un site pour l'arène, le
         moyen de sélection des joueurs. Nous devions également réaliser la boutique du club, où les maillots officiels
@@ -50,6 +50,72 @@
           <figcaption class="text-center">Boutique</figcaption>
         </figure>
       </div>
+      <p class="ml-5 mr-5 mt-5 text-gray-700 text-left text-xl mb-5">
+        Nous avions donc deux sites à réaliser : le site du club, et le site de la boutique. Même si la boutique ne
+        représentait qu'une page, nous avions à l'époque fait le choix avec l'association de la séparer du site du club,
+        afin de ne pas être trop limité en termes de rendu graphique (Woocommerce peut être limitant sur certains
+        aspects). De plus, nous voulions éviter une surcharge des plugins. Nous avons utilisé Woocommerce pour la
+        création de la plugin, et le site de la boutique était entièrement à ma charge.
+      </p>
     </div>
+
+    <div class="mb-12">
+      <h2 class="text-3xl font-bold mb-4 text-center">Entre la création et la refonte</h2>
+      <p class="ml-5 mr-5 text-gray-700 text-left text-xl mb-5">
+        Ensuite, les vacances séparant la première et la deuxième année sont arrivées. Je suis resté en contact avec
+        l'association, et suis devenu un membre à part entière, chargé du développement du site. Nous avons eu quelques
+        réunions pour discuter de la refonte de ce dernier, afin de ne pas garder trois sites différents.
+      </p>
+    </div>
+
+    <div class="mb-12">
+      <h2 class="text-3xl font-bold mb-4 text-center">Première refonte</h2>
+      <p class="ml-5 mr-5 text-gray-700 text-left text-xl mb-5">
+        Pour le troisième semestre, j'ai de nouveau choisi de travailler avec Unified Champions pour mon projet
+        collectif. Ayant participé aux réunions régulièrement, j'ai pris le rôle de chef de projet. Nous partions au
+        début sur deux sites distincts plut$pt que quatre : le site principal et la boutique. Puis il nous a été demandé
+        par le jury de ne faire qu'un seul site, ce que nous avons fait. Nous nous sommes basés sur un thème complexe
+        masi complet : Splash. Nous avions pour gérer le club et l'association l'extension l'extension Sportpress ; et
+        pour gérer la boutique toujours l'extension Woocommerce. Nous avons créé un thème enfant de Splash que nous
+        avons modifié en local.
+      </p>
+      <figure>
+        <img src="/img/ucS3.png" alt="Screenshot de la page équipe pour la refonte du site d'Unified Champions"
+          class="m-auto w-[60%]" />
+        <figcaption class="text-center">Page de l'équipe UC Targon</figcaption>
+      </figure>
+      <p class="ml-5 mr-5 mt-5 text-gray-700 text-left text-xl mb-5">
+        Nous gérions équipes et joueurs avec les outils fournis par Sportpress : ajouter une équipe ou un joueur met
+        automatiquement à jour l'affichage des pages. De même pour l'ajout d'un produit avec Woocommerce
+      </p>
+    </div>
+
+    <div class="mb-12">
+      <h2 class="text-3xl font-bold mb-4 text-center">Création des sites</h2>
+      <p class="ml-5 mr-5 text-gray-700 text-left text-xl mb-5">
+        Enfin, pour mon stage, j'ai choisi de le faire à l'association. Une de mes tâches a été de séparer les deux
+        sites : avec l'aide d'un designer, nous avons repensé les sites pour qu'ils gardent une cohérence graphique en
+        ayant des extensions différentes, et qu'ils soient toujours liés.
+      </p>
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-8 mt-5">
+        <figure>
+          <img src="/img/ucLol.png"
+            alt="Screenshot de la page League of legends de la version finale du site d'Unified Champions"
+            class="m-auto" />
+          <figcaption class="text-center">Page League of Legends</figcaption>
+        </figure>
+        <figure>
+          <img src="/img/maquetteBoutiqueUC.png"
+            alt="Screenshot de la maquette de la page d'accueil de la boutique pour la version finale du site d'Unified Champions"
+            class="m-auto" />
+          <figcaption class="text-center">Boutique</figcaption>
+        </figure>
+      </div>
+      <p class="ml-5 mr-5 mt-5 text-gray-700 text-left text-xl mb-5">
+        Au cours de ma troisième année, ce sont d'autres étudiants qui ont repris le projet de refonte du site. Je les
+        ai aidé quand c'était nécessaire en les renseignant du mieux que je pouvais.
+      </p>
+    </div>
+
   </div>
 </template>
